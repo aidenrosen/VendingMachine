fc85dc17ce7c687f3d80878ed44ef5e64ce5ccfa
aidenrosen <aidenrrosen@gmail.com>
2022-02-15 15:04:39 -0500

    Connected user input to the array of snacks

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,19 +1,55 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class VendingMachine
 {
+	static Scanner input;
+	static String[] snacks;
+	static double[] prices;
+	static int snackIndex;
+
 	public static void main(String[] args)
 	{
-		String[] snacks = {"Chips", "Cookies", "Gum", "Pretzels", "Crackers"};
-		double[] prices = {1.50, 1.50, 0.50, 1.00, 1.00};
+		snacks = new String[]{"Chips", "Cookies", "Gum", "Pretzels", "Crackers"};
+		prices = new double[]{1.50, 1.50, 0.50, 1.00, 1.00};
 
-		Scanner input = new Scanner(System.in);
+		input = new Scanner(System.in);
 		double money = promptMoney(input);
-		int snackIndex = promptSnack(input, snacks, prices);
+		snackIndex = promptSnack(input, snacks, prices);
+
+		if(money < prices[snackIndex])
+		{
+			System.out.println("You can't afford this item.  Please insert more money.");
+
+			//If the user is broke, run the whole program again to give them another chance.
+			main(args);
+		}
+		else confirmPurchase();
+
 
 		//TODO: Connect the user input to which snack they want with confirmation(commit 3)
-		//TODO: Subtract price of snack to amount given.  Give change and goodbye message if they have enough, give error if they don't.
+
 	}
+
+
+	public static void confirmPurchase()
+	{
+		System.out.println("Are you sure you want to buy " + snacks[snackIndex].toLowerCase() + "?  Type 'y' to confirm, 'n' to cancel.");
+		switch(input.next())
+		{
+			case "y":
+				//TODO: Subtract price of snack to amount given.  Give change and goodbye message if they have enough, give error if they don't.
+				break;
+			case "n":
+				System.out.println("Transaction cancelled.  Goodbye");
+				break;
+			default:
+				System.out.println("Invalid Input.  Try again.");
+				confirmPurchase();
+				break;
+		}
+	}
+
 
 	public static double promptMoney(Scanner input) {
 		while (true) {
